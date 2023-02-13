5a028f8b33180360102502c1a618f6b878b8763d
jss2a98aj <jss2a98aj@gmail.com>
2023-02-13 15:27:37 -0500

    Added FML plugin name to early mixins plugin

--- src/main/java/jss/notfine/mixinplugin/NotFineEarlyMixins.java
+++ src/main/java/jss/notfine/mixinplugin/NotFineEarlyMixins.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@IFMLLoadingPlugin.Name("NotFineEarlyMixins")
 @IFMLLoadingPlugin.MCVersion("1.7.10")
 public class NotFineEarlyMixins implements IFMLLoadingPlugin, IEarlyMixinLoader {
 
