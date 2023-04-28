fef2745011bf57c5c12b18c1bb76853e47b8f717
jss2a98aj <jss2a98aj@gmail.com>
2023-04-28 00:12:42 -0400

    Update buildscript

--- src/main/java/jss/notfine/mixins/early/minecraft/toggle/MixinRender.java
+++ src/main/java/jss/notfine/mixins/early/minecraft/toggle/MixinRender.java
@@ -3,10 +3,10 @@ package jss.notfine.mixins.early.minecraft.toggle;
 import jss.notfine.core.SettingsManager;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.settings.GameSettings;
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.libraries.org.objectweb.asm.Opcodes;
 
 @Mixin(value = Render.class)
 public abstract class MixinRender {
