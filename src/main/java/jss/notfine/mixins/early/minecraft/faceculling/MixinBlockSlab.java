75145a8c1d93a3b352e40c2f03ba9b7e79b14c0a
jss2a98aj <jss2a98aj@gmail.com>
2024-01-24 02:30:05 -0500

    Remove unused import

--- src/main/java/jss/notfine/mixins/early/minecraft/faceculling/MixinBlockSlab.java
+++ src/main/java/jss/notfine/mixins/early/minecraft/faceculling/MixinBlockSlab.java
@@ -1,6 +1,5 @@
 package jss.notfine.mixins.early.minecraft.faceculling;
 
-import cpw.mods.fml.relauncher.SideOnly;
 import jss.notfine.util.IFaceObstructionCheckHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSlab;
