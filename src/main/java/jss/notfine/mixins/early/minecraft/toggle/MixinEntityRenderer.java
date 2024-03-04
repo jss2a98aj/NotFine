ccbe6bc21ebbccc3ccfe0af05ba7bcca67ae9d82
jss2a98aj <jss2a98aj@gmail.com>
2024-03-03 22:28:59 -0500

    Add light flicker toggle

--- src/main/java/jss/notfine/mixins/early/minecraft/toggle/MixinEntityRenderer.java
+++ src/main/java/jss/notfine/mixins/early/minecraft/toggle/MixinEntityRenderer.java
@@ -1,11 +1,13 @@
 package jss.notfine.mixins.early.minecraft.toggle;
 
+import jss.notfine.core.Settings;
 import jss.notfine.core.SettingsManager;
 
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.settings.GameSettings;
 import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -21,8 +23,28 @@ abstract public class MixinEntityRenderer {
             ordinal = 0
         )
     )
-    private boolean notFine$toggleWaterDetail(GameSettings settings) {
+    private boolean toggleWaterDetail(GameSettings settings) {
         return SettingsManager.waterDetail;
     }
+
+    @Redirect(
+        method = "updateRenderer()V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/renderer/EntityRenderer;updateTorchFlicker()V"
+        )
+    )
+    private void toggleTorchFlicker(EntityRenderer instance) {
+        if((boolean)Settings.MODE_LIGHT_FLICKER.option.getStore()) {
+            updateTorchFlicker();
+        } else {
+            lightmapUpdateNeeded = true;
+        }
+    }
+
+    @Shadow
+    abstract void updateTorchFlicker();
+
+    @Shadow private boolean lightmapUpdateNeeded;
 
 }
