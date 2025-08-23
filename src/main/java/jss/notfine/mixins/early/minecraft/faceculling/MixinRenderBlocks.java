e1a0bf00a4e15108f2ab4068459134a1b6805ff0
jss2a98aj <jss2a98aj@gmail.com>
2025-08-23 17:27:34 -0400

    Make MixinRenderBlocks mixins less invasive

--- src/main/java/jss/notfine/mixins/early/minecraft/faceculling/MixinRenderBlocks.java
+++ src/main/java/jss/notfine/mixins/early/minecraft/faceculling/MixinRenderBlocks.java
@@ -1,5 +1,7 @@
 package jss.notfine.mixins.early.minecraft.faceculling;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockFence;
 import net.minecraft.block.BlockWall;
@@ -7,90 +9,71 @@ import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = RenderBlocks.class)
 public abstract class MixinRenderBlocks {
 
-    /**
-     * @author jss2a98aj
-     * @reason Do not render fence arms unless actually required.
-     */
-    @Overwrite
-    public boolean renderBlockFence(BlockFence fence, int x, int y, int z) {
-        float min = 0.375F;
-        float max = 0.625F;
-        setRenderBounds(min, 0.0D, min, max, 1.0D, max);
-        renderStandardBlock(fence, x, y, z);
-
-        boolean connectXNeg = fence.canConnectFenceTo(blockAccess, x - 1, y, z);
-        boolean connectXPos = fence.canConnectFenceTo(blockAccess, x + 1, y, z);
-        boolean connectZNeg = fence.canConnectFenceTo(blockAccess, x, y, z - 1);
-        boolean connectZPos = fence.canConnectFenceTo(blockAccess, x, y, z + 1);
-        boolean connectX = connectXNeg || connectXPos;
-        boolean connectZ = connectZNeg || connectZPos;
-
-        min = 0.4375F;
-        max = 0.5625F;
-        float minY = 0.75F;
-        float maxY = 0.9375F;
-        float minX = connectXNeg ? 0.0F : min;
-        float maxX = connectXPos ? 1.0F : max;
-        float minZ = connectZNeg ? 0.0F : min;
-        float maxZ = connectZPos ? 1.0F : max;
-        field_152631_f = true;
-        //Upper beam
-        if(connectX) {
-            setRenderBounds(minX, minY, min, maxX, maxY, max);
-            renderStandardBlock(fence, x, y, z);
+    @WrapOperation(
+        method = "renderBlockFence(Lnet/minecraft/block/BlockFence;III)Z",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/renderer/RenderBlocks;renderStandardBlock(Lnet/minecraft/block/Block;III)Z",
+            ordinal = 1
+        )
+    )
+    public boolean renderBlockFenceSkipArm1(RenderBlocks instance, Block blockType, int blockX, int blockY, int blockZ, Operation<Boolean> original) {
+        if(((BlockFence)blockType).canConnectFenceTo(blockAccess, blockX - 1, blockY, blockZ) || ((BlockFence)blockType).canConnectFenceTo(blockAccess, blockX + 1, blockY, blockZ)) {
+            return original.call(instance, blockType, blockX, blockY, blockZ);
         }
-        if(connectZ) {
-            setRenderBounds(min, minY, minZ, max, maxY, maxZ);
-            renderStandardBlock(fence, x, y, z);
-        }
+        return false;
+    }
 
-        minY = 0.375F;
-        maxY = 0.5625F;
-        //Lower beam
-        if(connectX) {
-            setRenderBounds(minX, minY, min, maxX, maxY, max);
-            renderStandardBlock(fence, x, y, z);
+    @WrapOperation(
+        method = "renderBlockFence(Lnet/minecraft/block/BlockFence;III)Z",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/renderer/RenderBlocks;renderStandardBlock(Lnet/minecraft/block/Block;III)Z",
+            ordinal = 3
+        )
+    )
+    public boolean renderBlockFenceSkipArm2(RenderBlocks instance, Block blockType, int blockX, int blockY, int blockZ, Operation<Boolean> original) {
+        if(((BlockFence)blockType).canConnectFenceTo(blockAccess, blockX - 1, blockY, blockZ) || ((BlockFence)blockType).canConnectFenceTo(blockAccess, blockX + 1, blockY, blockZ)) {
+            return original.call(instance, blockType, blockX, blockY, blockZ);
         }
-        if(connectZ) {
-            setRenderBounds(min, minY, minZ, max, maxY, maxZ);
-            renderStandardBlock(fence, x, y, z);
-        }
-
-        field_152631_f = false;
-        fence.setBlockBoundsBasedOnState(blockAccess, x, y, z);
-        return true;
+        return false;
     }
 
     /**
-     * @author jss2a98aj
+     * @author jss2a98aj, Roadhog360
      * @reason Cull faces against solid blocks, fix door smooth lighting
      */
-    @Overwrite
-    public boolean renderBlockDoor(Block block, int x, int y, int z) {
-        final int meta = blockAccess.getBlockMetadata(x, y, z);
+    @Inject(
+        at = @At("HEAD"),
+        method = "renderBlockDoor(Lnet/minecraft/block/Block;III)Z",
+        cancellable = true
+    )
+    public void renderBlockDoor(Block blockType, int blockX, int blockY, int blockZ, CallbackInfoReturnable<Boolean> cir) {
+        final int meta = blockAccess.getBlockMetadata(blockX, blockY, blockZ);
 
         if((meta & 8) != 0) {
-            if (blockAccess.getBlock(x, y - 1, z) != block) {
-                return false;
+            if (blockAccess.getBlock(blockX, blockY - 1, blockZ) != blockType) {
+                cir.setReturnValue(false);
             }
-        } else if (blockAccess.getBlock(x, y + 1, z) != block) {
-            return false;
+        } else if (blockAccess.getBlock(blockX, blockY + 1, blockZ) != blockType) {
+            cir.setReturnValue(false);
         }
 
         if ((meta & 8) != 0) {
             uvRotateTop = meta % 6;
         }
-        boolean flag = renderStandardBlock(block, x, y, z);
+        boolean flag = renderStandardBlock(blockType, blockX, blockY, blockZ);
         uvRotateTop = 0;
-        return flag;
+        cir.setReturnValue(flag);
     }
 
     /**
@@ -110,9 +93,7 @@ public abstract class MixinRenderBlocks {
     }
 
     @Shadow public IBlockAccess blockAccess;
-    @Shadow public boolean field_152631_f;
     @Shadow public int uvRotateTop;
-    @Shadow public abstract void setRenderBounds(double minX, double minY, double minZ, double maxX, double maxY, double maxZ);
     @Shadow public abstract boolean renderStandardBlock(Block blockType, int blockX, int blockY, int blockZ);
 
 }
